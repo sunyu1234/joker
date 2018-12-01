e8001b0e5e7be71b16694b33bdeaa0fcd7faf9a2
sy <3302274278@qq.com>
2018-12-01 10:31:03 +0800

    加7

--- system-dao/src/test/java/org/lanqiao/UserTest.java
+++ system-dao/src/test/java/org/lanqiao/UserTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 public class UserTest {
     @Test
     public void userTest(){
-        User user = new User("张三","123456");
+        User user = new User("张三","1234567");
         user.showInfo(user);
     }
 }
